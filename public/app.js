f46fd544e55c18dfd789c8364917dc501b0eccd2
binetougaye <fabigaye3@gmail.com>
2025-01-25 14:33:13 +0000

    feat: menu mobile

--- public/app.js
+++ public/app.js
@@ -2,8 +2,9 @@ const icons = document.querySelectorAll(".icon-arrow");
 const dropdowns = document.querySelectorAll(".dropdown");
 dropdowns.forEach((dropdown) => dropdown.classList.add("hidden"));
 icons.forEach((icon, index) => {
-    icon.addEventListener("mouseover", () => {
+    icon.addEventListener("click", () => {
         const dropdown = dropdowns[index]; // Associer l'icône au dropdown correspondant
+        console.log(dropdown);
 
         if (icon.getAttribute("src") === "./images/icon-arrow-down.svg") {
             icon.setAttribute("src", "./images/icon-arrow-up.svg");
@@ -16,3 +17,46 @@ icons.forEach((icon, index) => {
         }
     });
 });
+const mediaScreen = window.matchMedia('(max-width: 700px)')
+const iconMenu = document.getElementById("icon-menu")
+const navItems = document.querySelector(".nav-items")
+const auth = document.querySelector(".auth")
+console.log(auth);
+
+function mediaQuery(e) {
+    if (e.matches) {
+        iconMenu.classList.remove("hidden")
+        iconMenu.classList.add("visible")
+        auth.classList.add("hidden")
+        auth.classList.remove('visible')
+        navItems.classList.add("hidden")
+        navItems.classList.remove("visible")
+
+    } else {
+        iconMenu.classList.add("hidden")
+        iconMenu.classList.remove("visible")
+        auth.classList.add("visible")
+        auth.classList.remove("hidden")
+        navItems.classList.add("visible")
+        navItems.classList.remove("hidden")
+    }
+
+}
+const menuMobile = document.querySelector(".menu-mobile")
+const closeMenu = document.getElementById("close-menu")
+function mobile() {
+    menuMobile.classList.remove("hidden")
+    menuMobile.classList.add("visible")
+    iconMenu.classList.add("hidden")
+    iconMenu.classList.remove("visible")
+
+}
+iconMenu.addEventListener("click", mobile)
+closeMenu.addEventListener("click", () => {
+    menuMobile.classList.add("hidden")
+    menuMobile.classList.remove("visible")
+    iconMenu.classList.add("visible")
+    iconMenu.classList.remove("hidden")
+})
+mediaScreen.addEventListener("change", mediaQuery)
+mediaQuery(mediaScreen)
